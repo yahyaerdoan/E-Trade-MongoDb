6324dca956f096f3db4a590c408c32b1f231e1c7
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-06-26 11:24:35 -0500

    Chectout view designed.

---
+++ MongoDb.UserInterface/Controllers/CheckOutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MongoDb.UserInterface.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
